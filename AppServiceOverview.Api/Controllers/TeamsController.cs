e8af920a3c2bb519ab44d187b60be46dfb6cbd80
Michael Weinand <mike@michaelweinand.com>
2015-11-01 18:57:36 -0600

    initial api and web apps

---
+++ AppServiceOverview.Api/Controllers/TeamsController.cs
@@ -0,0 +1,20 @@
+﻿using AppServiceOverview.Api.Models;
+using System;
+using System.Web.Http;
+
+namespace AppServiceOverview.Api.Controllers
+{
+    public class TeamsController : ApiController
+    {
+        public IHttpActionResult Get(int id)
+        {
+            var random = new Random();
+            var result = new TeamModel
+            {
+                TeamNumber = id,
+                Rank = random.Next(1, 40)
+            };
+            return Ok(result);
+        }
+    }
+}
---
+++ AppServiceOverview.Web/Controllers/RankController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppServiceOverview.Web.Controllers
+{
+    public class RankController : ApiController
+    {
+
+        public IHttpActionResult GetTeamRank(int id)
+        {
+            return Ok(5);
+        }
+    }
+}
